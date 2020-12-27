b5e6119f3d93791aa86dc3a0891e05ce0b833eb9
Todd James <todd@itodd.co>
2020-12-27 15:02:31 -0500

    React Router Setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import pages
+import Home from './pages/Home';
+
 import './App.css';
 
 function App() {
-  return <div>Neighborhood Meeting</div>;
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="*">
+            <Error />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
